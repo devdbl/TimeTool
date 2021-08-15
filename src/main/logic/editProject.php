c4a8040d3437ed33e9cc4e005074a96ce8400a44
devdbl <dominic.blattner@stud.abbts.ch>
2021-08-15 15:38:18 +0200

    MVC

--- src/main/logic/editProject.php
+++ src/main/logic/editProject.php
@@ -1,5 +1,7 @@
 <?php
 require_once("../controllers/DatabaseController.php");
+
 $db = new DatabaseController;
 $db->SelectAll("project");
+
 
--- src/main/logic/newProject.php
+++ src/main/logic/newProject.php
@@ -3,7 +3,7 @@
     require_once("../controllers/ProjectController.php");
 
     $NewProject = new ProjectController();
-
     $NewProject->newProject();
-echo "<b>Projekt gespeichert</b><br>";
-echo "<a href='../index.html'>go back</a>";
+
+    echo "<b>Projekt gespeichert</b><br>";
+    echo "<a href='../index.html'>go back</a>";
