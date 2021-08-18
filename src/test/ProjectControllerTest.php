feacdd186eef64761fbb41e9e79234d11f77954f
devdbl <82320831+devdbl@users.noreply.github.com>
2021-08-18 13:44:19 +0200

    Update ProjectControllerTest.php

--- src/test/ProjectControllerTest.php
+++ src/test/ProjectControllerTest.php
@@ -9,7 +9,7 @@ class ProjectControllerTest extends \PHPUnit\Framework\TestCase
 {
     public function testDivide()
     {
-        $simple = new Simple(10);
+        $simple = new \src\main\tools\Simple(10);
         $result = $simple->divide(2);
 
         $this->assertEquals(5, $result);
