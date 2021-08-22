a1200cffab6b5490e3835d68e506caa947f111e5
devdbl <dominic.blattner@stud.abbts.ch>
2021-08-22 14:51:07 +0200

    Update logout.php

--- src/main/logout.php
+++ src/main/logout.php
@@ -1,2 +1,19 @@
 <?php
-echo '<b>bye-bye</b>';
+require '../../vendor/autoload.php';
+use Auth0\SDK\Auth0;
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+$auth0 = new Auth0([
+    'domain' => $_ENV['AUTH0_DOMAIN'],
+    'client_id' => $_ENV['AUTH0_CLIENT_ID'],
+    'client_secret' => $_ENV['AUTH0_CLIENT_SECRET'],
+    'redirect_uri' => $_ENV['AUTH0_CALLBACK_URL'],
+    'scope' => 'openid profile email',
+]);
+
+$auth0->logout();
+
+echo '<b>bye-bye</b><br>';
+echo '<a href="index.php">LandingPage</a>';
