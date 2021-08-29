851dbd080491c5e0d13ac857ea00b451a4b7bff8
devdbl <dominic.blattner@stud.abbts.ch>
2021-08-29 16:27:44 +0200

    Update login.php

--- src/main/views/login.php
+++ src/main/views/login.php
@@ -22,29 +22,35 @@ if(isset($_GET['login'])) {
     //Überprüfung des Passworts
     if ($user !== false && password_verify($password, $user['PASSWORD'])) {
         $_SESSION['userid'] = $user['EMPLOYEE_ID'];
+        $_SESSION['name']   = $user['FIRSTNAME'];
         $_SESSION['isAdmin']= $user['ROLE'];
-        die('Login erfolgreich. Weiter zu <a href="../index.php">internen Bereich</a>');
+        die('Login erfolgreich. Weiter zu <a href="index.php">internen Bereich</a>'
+            /*header('Location: https://localhost/index.php')*/);
     } else {
         $errorMessage = "Kürzel oder Passwort war ungültig<br>";
     }
 
 }
 
-if(isset($errorMessage)) {
-    echo $errorMessage;
-}
 echo $header;
 echo $navbar;
-echo    '<form>
-            <div class="inhalt">
-                <h1>Login</h1><p></p>
-                    <div class="form-group">
-                        <label for="shortname">Benutzername</label>
-                        <input type="text" class="form-control" id="shortname">
-                    </div>
-                    <div class="form-group">
-                        <label for="pwd">Passwort</label>
-                        <input type="password" class="form-control" id="pwd">
-                    </div>
-                    <input type="submit" class="btn btn-info" value="Login">
-        </form>';
+
+if(isset($errorMessage)) {
+    echo '<div class = "error"><mark>'.$errorMessage.'</mark></div>';
+}
+
+echo    '
+        <div class="inhalt">
+            <h1>Login</h1>
+            <form action="?login=1" method="post">
+                <div class="form-group">
+                    <label for="id">Benutzername</label>
+                    <input type="test" class="form-control" id="id" name = "shortname">
+                </div>
+                <div class="form-group">
+                    <label for="pwd">Passwort</label>
+                    <input type="password" class="form-control" id="pwd" name = "password">
+                </div>
+                <input type="submit" class="btn btn-info" value="Login">
+            </form>
+        </div>';
