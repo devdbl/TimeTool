edda67771874547296842724613bd4a54d845654
devdbl <dominic.blattner@stud.abbts.ch>
2021-08-29 15:27:15 +0200

    Create logout.php

---
+++ src/main/views/logout.php
@@ -0,0 +1,17 @@
+<?php
+require_once ("Helper.php");
+
+session_start();
+session_destroy();
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Registrierung</title>
+    <link rel="stylesheet" href="./views/css/app.css">
+</head>
+<body>
+"Logout erfolgreich"
+</body>
+</html>
