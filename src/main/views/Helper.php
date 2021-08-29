c98d76447c08c35ebb5e52562979e4b55d3bfc4c
devdbl <dominic.blattner@stud.abbts.ch>
2021-08-29 20:33:03 +0200

    Update Helper.php

--- src/main/views/Helper.php
+++ src/main/views/Helper.php
@@ -13,6 +13,7 @@ class Helper
                         <meta name="viewport" content="width=device-width, initial-scale=1.0">
                         <link href="./CSS/layout.css" type="text/css" rel="stylesheet" />
                         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+                        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
                         <title>' . $title . '</title>
                     </head>
                     <body>
@@ -33,21 +34,16 @@ class Helper
                                   </span>
                     
                     
-                    </nav>
-                     <!-- Load an icon library -->
-                    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">    
-                    <!-- versuch Bild einzufügen-->
+                    </nav>       
+                    <!--Bild einzufügen-->
                     <div class="logo">
-                        <img src="../../frontend/Kernkraftwer_Goesgen_Daeniken_AG.jpg" alt="Firmen Logo" class="img-fluid" width="300" >
+                        <img src="./resources/Kernkraftwer_Goesgen_Daeniken_AG.jpg" alt="Firmen Logo" class="img-fluid" width="300" >
                     </div>';
         return $navbar;
     }
 
     public static function getSidebar(){
-        $sidebar = '<!-- Load an icon library -->
-                     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
-                    
-                     <!-- The sidebar -->
+        $sidebar = '<!-- The sidebar -->
                      <div class="sidebar">
                         <a href="zeiterfassung_startpage.php"><i class="fa fa-fw fa-clock"></i> Zeiterfassung</a>
                         <a href="projektverwaltung_startpage.php"><i class="fa fa-fw fa-wrench"></i> Projekt Verwaltung</a>
