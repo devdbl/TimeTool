a023122847acb48542c4c74e1baf7a525f33e972
devdbl <dominic.blattner@stud.abbts.ch>
2021-08-29 17:10:22 +0200

    Update mitarbeiterverwaltung_bearbeiten.php

--- src/main/views/mitarbeiterverwaltung_bearbeiten.php
+++ src/main/views/mitarbeiterverwaltung_bearbeiten.php
@@ -15,30 +15,35 @@ if(!isset($_SESSION['userid'])) {
     die('<div class="inhalt">Bitte zuerst <a href="login.php">einloggen</a></div>');
 }else {
     echo $sidebar;
-    echo '<form>
-            <div class="inhalt">
-                <h1>Bitte erfassen sie einen neuen Mitarbeiter</h4><p></p>
+    echo '
+          <div class="inhalt">
+            <h1>Bitte erfasse deine Änderungen</h1>
+                <form>
                     <div class="form-group">
-                        <label for="usr">Vorname</label>
-                        <input type="text" class="form-control" id="usr">
-                      </div>
-                      <div class="form-group">
-                        <label for="pwd">Nachname</label>
+                        <label for="firstname">Vorname</label>
+                        <input type="text" class="form-control" id="firstname">
+                    </div>
+                    <div class="form-group">
+                        <label for="lastname">Nachname</label>
+                        <input type="text" class="form-control" id="lastname">
+                    </div>
+                    <div class="form-group">
+                        <label for="pwd">Passwort</label>
                         <input type="text" class="form-control" id="pwd">
-                      </div>
-                      <div class="form-group">
-                        <label for="pwd">Mitarbeiter ID</label>
-                        <input type="text" class="form-control" id="pwd">
-                      </div>
-                      <div class="form-group">
-                        <label for="pwd">E-Mail</label>
-                        <input type="text" class="form-control" id="pwd">
-                      </div>
-                      <div class="custom-control custom-checkbox mb-3">
+                    </div>
+                    <div class="form-group">
+                        <label for="pwd2">Passwort wiederholen</label>
+                        <input type="text" class="form-control" id="pwd2">
+                    </div>
+                    <div class="custom-control custom-checkbox mb-3">
                         <input type="checkbox" class="custom-control-input" id="customCheck" name="example1">
-                        <label class="custom-control-label" for="customCheck">Mitarbeiter Aktiv</label>
-                      </div>
-                      <input type="submit" class="btn btn-info" value="Speichern">
+                        <label class="custom-control-label" for="customCheck">Administrator</label>
+                    </div>
+                    <div class="custom-control custom-checkbox mb-3">
+                        <input type="checkbox" class="custom-control-input" id="customCheck" name="example1">
+                        <label class="custom-control-label" for="customCheck">Mitarbeiter deaktivieren</label>
+                    </div>
+                    <input type="submit" class="btn btn-info" value="Speichern">
                 </form>';
 
     echo $helper->getFooter();
