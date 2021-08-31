be45ee63294f0e7feca602fb0fd3dc1ff98b0636
devdbl <dominic.blattner@stud.abbts.ch>
2021-08-31 20:52:45 +0200

    Update mitarbeiterverwaltung_bearbeiten.php

--- src/main/views/mitarbeiterverwaltung_bearbeiten.php
+++ src/main/views/mitarbeiterverwaltung_bearbeiten.php
@@ -7,6 +7,7 @@ $helper = new Helper();
 $header = $helper->getHeader("Mitarbeiter verwalten");
 $navbar = $helper->getNavbar();
 $sidebar = $helper->getSidebar();
+$footer = $helper->getFooter();
 
 echo $header;
 echo $navbar;
@@ -34,17 +35,19 @@ if(!isset($_SESSION['userid'])) {
                     <div class="form-group">
                         <label for="pwd2">Passwort wiederholen</label>
                         <input type="text" class="form-control" id="pwd2">
-                    </div>
-                    <div class="custom-control custom-checkbox mb-3">
-                        <input type="checkbox" class="custom-control-input" id="customCheck" name="example1">
-                        <label class="custom-control-label" for="customCheck">Administrator</label>
-                    </div>
-                    <div class="custom-control custom-checkbox mb-3">
-                        <input type="checkbox" class="custom-control-input" id="customCheck" name="example1">
-                        <label class="custom-control-label" for="customCheck">Mitarbeiter deaktivieren</label>
-                    </div>
-                    <input type="submit" class="btn btn-info" value="Speichern">
-                </form>';
+                    </div>';
+    if($_SESSION['admin'==1]){
+        echo    '<div class="custom-control custom-checkbox mb-3">
+                    <input type="checkbox" class="custom-control-input" id="customCheck" name="example1">
+                    <label class="custom-control-label" for="customCheck">Administrator</label>
+                </div>
+                <div class="custom-control custom-checkbox mb-3">
+                    <input type="checkbox" class="custom-control-input" id="customCheck" name="example1">
+                    <label class="custom-control-label" for="customCheck">Mitarbeiter deaktivieren</label>
+                </div>';
+    }
+    echo    '<input type="submit" class="btn btn-info" value="Speichern">
+            </form>';
 
-    echo $helper->getFooter();
+    echo $footer;
 }
