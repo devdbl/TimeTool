c7707dfad55b791440d2c69255bf7e51eb910706
devdbl <dominic.blattner@stud.abbts.ch>
2021-09-10 00:18:50 +0200

    Create ReportTest.php

---
+++ src/test/integrationTest/ReportTest.php
@@ -0,0 +1,32 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+require_once("src/main/controllers/ReportController.php");
+require_once("src/main/tools/DatabaseConnector.php");
+
+class ReportTest extends TestCase
+{
+    public function testCreateReport(){
+        $sum = 0;
+        $projectId = null;
+        $userId = null;
+        $dateArray['startDate'] = "2021-08-01";
+        $dateArray['endDate'] = "2021-08-01";
+        $overview = 1;
+        $requestMethod = 'WEB';
+
+
+        $dbConnection = (new DatabaseConnector())->connect();
+
+        $reportController = new ReportController($dbConnection, $requestMethod, $projectId, $userId, $dateArray, $overview);
+        $reportController->processRequest();
+
+        foreach($reportController->dataArray as $row){
+            $sum++;
+        }
+
+        $this->assertEquals(2, $sum);
+    }
+
+}
